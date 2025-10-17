d5b96fb84d9e262c65a3362b345594d094a807a8
vijaysahu1111 <vijaysahumodi1111@gmail.com>
2025-10-17 13:26:28 +0530

    pazzle game

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div className="container-fluid bg-dark vh-100 w-100" >
+      <div className=" container mini-box d-flex flex-wrap">
+       <div className="box ulta-mini-box d-flex"> 1</div>
+       <div className="box ulta-mini-box d-flex"> 2</div>
+       <div className="box ulta-mini-box d-flex"> 3</div>
+       <div className="box ulta-mini-box d-flex"> 4</div>
+       <div className="box ulta-mini-box d-flex"> 5</div>
+       <div className="box ulta-mini-box d-flex"> 6</div>
+       <div className="box ulta-mini-box d-flex"> 7</div>
+       <div className="box ulta-mini-box d-flex"> 8</div>
+       <div className="box ulta-mini-box d-flex"> 9</div>
+       <div className="box ulta-mini-box d-flex"> 10</div>
+       <div className="box ulta-mini-box d-flex"> 11</div>
+       <div className="box ulta-mini-box d-flex"> 12</div>
+       <div className="box ulta-mini-box d-flex"> 13</div>
+       <div className="box ulta-mini-box d-flex"> 14</div>
+       <div className="box ulta-mini-box d-flex"> 15</div>
+       <div className="box ulta-mini-box d-flex"> 16</div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
+
